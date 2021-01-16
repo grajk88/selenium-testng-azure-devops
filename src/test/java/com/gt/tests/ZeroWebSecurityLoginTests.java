36a5fa4c93d0052b93cbc177e4cb976298514224
grajk88 <30874855+grajk88@users.noreply.github.com>
2021-01-16 13:13:38 +0000

    Added Screenshot code

--- src/test/java/com/gt/tests/ZeroWebSecurityLoginTests.java
+++ src/test/java/com/gt/tests/ZeroWebSecurityLoginTests.java
@@ -1,6 +1,13 @@
 package com.gt.tests;
 
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -9,6 +16,9 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+import ru.yandex.qatools.ashot.Screenshot;
 
 public class ZeroWebSecurityLoginTests {
 
@@ -41,6 +51,11 @@ public class ZeroWebSecurityLoginTests {
 
 			Thread.sleep(5000);
 
+			File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir") + "/target/images/HomePage.jpg"));
+
+			System.out.println("Added Screenshot to : " + System.getProperty("user.dir") + "/target/images/HomePage.jpg");
+
 			driver.findElement(By.xpath("//*[@id=\"settingsBox\"]/ul/li[3]")).click();
 			driver.findElement(By.linkText("Logout")).click();
 
@@ -50,7 +65,7 @@ public class ZeroWebSecurityLoginTests {
 		}
 
 	}
-	
+
 	@Test
 	public void loginTestTwo() {
 		try {
@@ -72,7 +87,7 @@ public class ZeroWebSecurityLoginTests {
 		}
 
 	}
-	
+
 	@Test
 	public void loginTestThree() {
 		try {
